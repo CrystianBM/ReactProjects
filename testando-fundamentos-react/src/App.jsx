52c71869ab624991ca47702f244d2a64cf753965
Crystian <cbm2003x1@gmail.com>
2023-03-22 16:53:55 -0300

    CARDS DONE

---
+++ testando-fundamentos-react/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import React from "react";
+
+import Card from "./components/layout/Card";
+import RandomGen from "./components/basics/RandomGen";
+import First from './components/basics/First';
+
+export default function App() {
+
+  return (
+    <div className="App"> 
+      <h1> Fundamentos React </h1>
+      <div className="cards">
+        <Card titulo="Random Challenge" >
+          <RandomGen min={1} max={60} />
+        </Card>
+
+        <Card titulo="First Component" >
+          <First />
+        </Card>
+      </div>
+    </div>
+  );
+}
