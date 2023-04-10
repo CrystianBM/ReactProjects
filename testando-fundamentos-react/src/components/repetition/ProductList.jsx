62e8924c610110bffd834e6a11f0d04ff02e8de0
Crystian <cbm2003x1@gmail.com>
2023-04-10 16:54:29 -0300

    .

--- testando-fundamentos-react/src/components/repetition/ProductList.jsx
+++ testando-fundamentos-react/src/components/repetition/ProductList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import products from '../../data/products';
+import "./ProductList.css";
 
 export default props => {
     const productList = products.map(product => {
-        return <tr key={product.id}>
+        return <tr key={product.id} className={product.id % 2 === 0 ? "par" : "impar"}>
             <td> {product.id} </td>
             <td> {product.name} </td>
-            <td> {product.price} </td>
+            <td> ${product.price.toFixed(2).replace('.', ',')} </td>
         </tr>
     })
     
